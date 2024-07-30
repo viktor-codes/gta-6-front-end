daac454860b57ffd0cde469bbad69ed61f5ce903
Viktor Rura <ruravictor@gmail.com>
2024-07-30 16:10:21 +0100

    Add animation to the form

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,22 +1,22 @@
 import styles from './Home.module.css'
 
-const isSuccess = true
+const isSuccess = false
 
 function Home() {
 	return (
 		<div className={styles.wrapper}>
-			{isSuccess ? (
-				<div className={styles.success}>
-					<h1>The Form Sent!</h1>
-				</div>
-			) : (
-				<form action=''>
-					<h1>GTA 6 - Leave your application</h1>
-					<input type='name' placeholder='Enter name:' />
-					<input type='email' placeholder='Enter email:' />
-					<button>Order</button>
-				</form>
-			)}
+			<form>
+				{isSuccess ? (
+					<div className={styles.success}>The Form Sent!</div>
+				) : (
+					<>
+						<h1>GTA 6 - Leave your application</h1>
+						<input type='name' placeholder='Enter name:' />
+						<input type='email' placeholder='Enter email:' />
+						<button>Order</button>
+					</>
+				)}
+			</form>
 		</div>
 	)
 }
