3e902a08b83d39edde771dbebde25fff2eecd444
Viktor Rura <ruravictor@gmail.com>
2024-07-30 16:01:53 +0100

    Add conditions to the form

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,14 +1,22 @@
 import styles from './Home.module.css'
 
+const isSuccess = true
+
 function Home() {
 	return (
 		<div className={styles.wrapper}>
-			<form action=''>
-				<h1>GTA 6 - Leave your application</h1>
-				<input type='name' placeholder='Enter name' />
-				<input type='email' placeholder='Enter email' />
-				<button>Order</button>
-			</form>
+			{isSuccess ? (
+				<div className={styles.success}>
+					<h1>The Form Sent!</h1>
+				</div>
+			) : (
+				<form action=''>
+					<h1>GTA 6 - Leave your application</h1>
+					<input type='name' placeholder='Enter name:' />
+					<input type='email' placeholder='Enter email:' />
+					<button>Order</button>
+				</form>
+			)}
 		</div>
 	)
 }
