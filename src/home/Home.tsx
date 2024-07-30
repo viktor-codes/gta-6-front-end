a2430efd41a45437a1a731fbd29857ec71564053
Viktor Rura <ruravictor@gmail.com>
2024-07-30 16:22:49 +0100

    Add form hook to the form

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,18 +1,37 @@
+import { SubmitHandler, useForm } from 'react-hook-form'
 import styles from './Home.module.css'
 
 const isSuccess = false
 
+interface IFormState {
+	name: string
+	email: string
+}
+
 function Home() {
+	const { register, handleSubmit } = useForm<IFormState>()
+
+	const onSubmit: SubmitHandler<IFormState> = data => {
+		console.log(data)
+	}
 	return (
 		<div className={styles.wrapper}>
-			<form>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				{isSuccess ? (
 					<div className={styles.success}>The Form Sent!</div>
 				) : (
 					<>
 						<h1>GTA 6 - Leave your application</h1>
-						<input type='name' placeholder='Enter name:' />
-						<input type='email' placeholder='Enter email:' />
+						<input
+							type='name'
+							placeholder='Enter name:'
+							{...register('name')}
+						/>
+						<input
+							type='email'
+							placeholder='Enter email:'
+							{...register('email')}
+						/>
 						<button>Order</button>
 					</>
 				)}
