9620089285410d8692c6b8c19aa7f173ffc6df2c
Viktor Rura <ruravictor@gmail.com>
2024-07-30 18:30:49 +0100

    Complete the functionality of sending the for to backend

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import styles from './Home.module.css'
 
@@ -9,10 +10,31 @@ interface IFormState {
 }
 
 function Home() {
-	const { register, handleSubmit } = useForm<IFormState>()
+	const { register, handleSubmit, reset } = useForm<IFormState>()
+	const [isLoading, setIsLoading] = useState(false)
+	const [isSuccess, setIsSuccess] = useState(false)
 
 	const onSubmit: SubmitHandler<IFormState> = data => {
-		console.log(data)
+		setIsLoading(true)
+		fetch('http://localhost:5000/api', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		})
+			.then(response => {
+				response.json()
+			})
+			.then(data => {
+				if (!data) return
+
+				setIsSuccess(true)
+				reset()
+			})
+			.finally(() => {
+				setIsLoading(false)
+			})
 	}
 	return (
 		<div className={styles.wrapper}>
@@ -32,7 +54,9 @@ function Home() {
 							placeholder='Enter email:'
 							{...register('email')}
 						/>
-						<button>Order</button>
+						<button disabled={isLoading}>
+							{isLoading ? 'Loading ...' : 'Order'}
+						</button>
 					</>
 				)}
 			</form>
