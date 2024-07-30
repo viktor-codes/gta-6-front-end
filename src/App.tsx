8c1cc178a3fef2bd12fb2a893c45ef6674e73fb5
Viktor Rura <ruravictor@gmail.com>
2024-07-30 14:28:21 +0100

    Decompose home app component to home

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import './App.css'
-
-function App() {
-	return <div className='text-6xl'>GTA 6 - Leave your application</div>
-}
-
-export default App
---
+++ src/home/Home.tsx
@@ -0,0 +1,16 @@
+import './Home.module.css'
+
+function Home() {
+	return (
+		<div>
+			<form action=''>
+				<h1>GTA 6 - Leave your application</h1>
+				<input type='name' placeholder='Enter name' />
+				<input type='email' placeholder='Enter email' />
+				<button>Order</button>
+			</form>
+		</div>
+	)
+}
+
+export default Home
